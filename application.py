7e0c6feb528099226f73bf951dac6650e91454c6
vagrant <vagrant@vagrant.vm>
2018-10-21 22:57:06 +0000

    Fixed some minor issues and added docstring comments

--- application.py
+++ application.py
@@ -1,4 +1,12 @@
-from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    jsonify,
+    url_for,
+    flash
+)
 # from sqlalchemy import create_engine, asc, desc
 from sqlalchemy import asc, desc
 # from sqlalchemy.orm import sessionmaker
@@ -25,6 +33,9 @@ db = SQLAlchemy(app)
 
 @app.context_processor
 def inject_dict_for_all_templates():
+    """
+    Injects username and user_id into all templates
+    """
     if 'username' in login_session and 'user_id' in login_session:
         return dict(username=login_session['username'],
                     user_id=login_session['user_id'])
@@ -34,6 +45,10 @@ def inject_dict_for_all_templates():
 
 @app.route('/login')
 def showLogin():
+    """
+    Shows login page with Google login button and provides an
+    anti-forgery state token for added security
+    """
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in range(32))
     login_session['state'] = state
@@ -42,6 +57,11 @@ def showLogin():
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
+    """
+    Handles login with Google- trades authorization code for credentials
+    object/ access token and validates. Will create a new user if not
+    exists already in database and stores login data in session.
+    """
     # Validate state token
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
@@ -126,8 +146,10 @@ def gconnect():
     output += '!</h1>'
     output += '<img src="'
     output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius:'
-    output += '150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    output += ' " style = "width: 300px; '
+    output += 'height: 300px;border-radius:'
+    output += '150px;-webkit-border-radius: '
+    output += '150px;-moz-border-radius: 150px;"> '
     flash("you are now logged in as %s" % login_session['username'])
     print("done!")
     return output
@@ -136,6 +158,10 @@ def gconnect():
 # DISCONNECT - Revoke a current user's token and reset their login_session
 @app.route('/gdisconnect')
 def gdisconnect():
+    """
+    Used for logging out- revokes access token and removes user data
+    from session
+    """
     # Only disconnect a connected user.
     access_token = login_session.get('access_token')
     if access_token is None:
@@ -170,6 +196,10 @@ def gdisconnect():
 @app.route('/')
 @app.route('/catalog')
 def showCatalogHome():
+    """
+    Shows catalog home page with all categories and 10 most recent
+    items
+    """
     categories = db.session.query(Category).order_by(asc(Category.name))
     items = db.session.query(Item).order_by(desc(Item.id)).limit(10)
     print(items[0].category.name)
@@ -180,6 +210,9 @@ def showCatalogHome():
 @app.route('/JSON/')
 @app.route('/catalog/JSON/')
 def showCatalogJson():
+    """
+    Returns JSON of all categories and their items
+    """
     categoryList = []
     categories = db.session.query(Category).order_by(asc(Category.name))
     for category in categories:
@@ -199,6 +232,9 @@ def showCatalogJson():
 
 @app.route('/catalog/<string:category_name>/items/')
 def showCategory(category_name):
+    """
+    Shows a chosen category page and its items
+    """
     categories = db.session.query(Category).order_by(asc(Category.name))
     selectedCategory = db.session.query(Category).\
         filter_by(name=category_name).one()
@@ -211,6 +247,9 @@ def showCategory(category_name):
 
 @app.route('/catalog/<string:category_name>/items/JSON/')
 def showCategoryJson(category_name):
+    """
+    Shows JSON for a chosen category and its items
+    """
     selectedCategory = db.session.query(Category).\
         filter_by(name=category_name).one()
     items = db.session.query(Item).\
@@ -228,6 +267,10 @@ def showCategoryJson(category_name):
 
 @app.route('/catalog/<string:category_name>/<string:item_name>/')
 def showItem(category_name, item_name):
+    """
+    Shows an item page with option to edit and delete if it was
+    created by the logged in user
+    """
     item, category = db.session.query(Item, Category).\
         filter(Item.category_id == Category.id).\
         filter(Category.name == category_name, Item.name == item_name).one()
@@ -237,6 +280,9 @@ def showItem(category_name, item_name):
 
 @app.route('/catalog/<string:category_name>/<string:item_name>/JSON/')
 def showItemJson(category_name, item_name):
+    """
+    Returns JSON for a chosen item
+    """
     item, category = db.session.query(Item, Category).\
         filter(Item.category_id == Category.id).\
         filter(Category.name == category_name, Item.name == item_name).one()
@@ -245,6 +291,12 @@ def showItemJson(category_name, item_name):
 
 @app.route('/catalog/item/new/', methods=['GET', 'POST'])
 def addItem():
+    """
+    For GET request, shows form to add a new item. On POST
+    request, will add a new item and then redirect to catalog
+    home page. Requires user to be logged in or will redirect
+    to login page.
+    """
     if 'username' not in login_session:
         return redirect('/login')
 
@@ -266,15 +318,23 @@ def addItem():
 @app.route('/catalog/<string:category_name>/<string:item_name>/edit/',
            methods=['GET', 'POST'])
 def editItem(category_name, item_name):
+    """
+    For GET request, shows form to edit an item. On POST
+    request, will save edited item and then redirect to catalog
+    home page. If user not logged in, will redirect to login
+    page. If user is not creator of item, will send alert message
+    that they are not authorized to edit item.
+    """
     if 'username' not in login_session:
         return redirect('/login')
-    editedItem, category = item, category = db.session.query(Item, Category).\
+    editedItem, _ = db.session.query(Item, Category).\
         filter(Item.category_id == Category.id).\
         filter(Category.name == category_name, Item.name == item_name).one()
     if login_session['user_id'] != editedItem.user_id:
-        return ("<script>function myFunction() {alert('You are not authorized" +
-        " to edit items you did not create.');}</script>" +
-        "<body onload='myFunction()''>")
+        return ("<script>function myFunction() " +
+                "{alert('You are not authorized" +
+                " to edit items you did not create.');}</script>" +
+                "<body onload='myFunction()''>")
     if request.method == 'POST':
         if request.form['itemName']:
             editedItem.name = request.form['itemName']
@@ -291,16 +351,24 @@ def editItem(category_name, item_name):
 @app.route('/catalog/<string:category_name>/<string:item_name>/delete/',
            methods=['GET', 'POST'])
 def deleteItem(category_name, item_name):
+    """
+    For GET request, shows button to confirm delete of an item. On
+    POST request, will delete item and then redirect to catalog
+    home page. If user not logged in, will redirect to login
+    page. If user is not creator of item, will send alert message
+    that they are not authorized to delete item.
+    """
     if 'username' not in login_session:
         return redirect('/login')
-    itemToDelete, category = item, category = db.session.\
+    itemToDelete, _ = db.session.\
         query(Item, Category).\
         filter(Item.category_id == Category.id).\
         filter(Category.name == category_name, Item.name == item_name).one()
     if login_session['user_id'] != itemToDelete.user_id:
         return ("<script>function myFunction() {alert('You are not" +
-        " authorized to delete items you did not create.');}</script>" +
-        "<body onload='myFunction()''>")
+                " authorized to delete items you did not create.');}" +
+                "</script>" +
+                "<body onload='myFunction()''>")
     if request.method == 'POST':
         db.session.delete(itemToDelete)
         db.session.commit()
@@ -312,6 +380,9 @@ def deleteItem(category_name, item_name):
 
 # User Helper Functions
 def createUser(login_session):
+    """
+    Adds new user to database
+    """
     newUser = User(name=login_session['username'], email=login_session[
                    'email'])
     db.session.add(newUser)
@@ -321,11 +392,17 @@ def createUser(login_session):
 
 
 def getUserInfo(user_id):
+    """
+    Gets user from database by id
+    """
     user = db.session.query(User).filter_by(id=user_id).one()
     return user
 
 
 def getUserID(email):
+    """
+    Gets user from database by email
+    """
     try:
         user = db.session.query(User).filter_by(email=email).one()
         return user.id
